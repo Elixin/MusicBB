f573861a474e64aa3f6a4d6878fc3634fa655825
elis <huyixin@163.com>
2024-01-11 10:53:43 +0800

    搜索框

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,13 @@ import ShringkingBar from './ui/shrinking-bar/shrinking-bar'
 export default function Home() {
 
   return (
+    // <div>
+    //   <div className="absolute z-40 ml-0 mt-0 bg-gray-400">z-40</div>
+    //     <div className="absolute z-30 ml-2 mt-2 bg-gray-500">z-30</div>
+    //     <div className="absolute z-20 ml-4 mt-4 bg-gray-600">z-20</div>
+    //     <div className="absolute z-10 ml-6 mt-6 bg-gray-700">z-10</div>
+    //     <div className="absolute z-0 ml-8 mt-8 bg-gray-800">z-0</div>
+    // </div>
     <div className='flex'>
       <div className='w-[16%] h-screen bg-gray-50'>
         <Nav />
--- src/app/ui/shrinking-bar/shrinking-bar.tsx
+++ src/app/ui/shrinking-bar/shrinking-bar.tsx
@@ -1,13 +1,52 @@
- export default function ShringkingBar() {
-    return(
-        <div className="flex ">
+'use client';
+
+import SearchIcon from "@/svg/search.svg"
+import { useRef, useState } from "react";
+
+export default function ShringkingBar() {
+    return (
+        <div className="flex w-screen items-center  h-24">
             <div>
-                <div></div>
+                <div className="w-20"></div>
             </div>
+            <SearchInput/>
             <div>
-                <input />
             </div>
-            <div></div>
         </div>
     )
- }
+}
+
+function SearchInput() {
+    let [promptStatus, setPromptStatus] = useState(true)
+    let inputRef = useRef<HTMLInputElement>(null)
+    
+    // 隐藏提示语，并默认选中提示语
+    function hiddenPrompt(event: any) {
+        console.log(event);
+        setPromptStatus(false)
+        if (inputRef.current != null) {
+            inputRef.current.focus()
+        }
+    }
+    // 当输入框内无值时显示提示语
+    function showPrompt() {
+        if (inputRef.current?.value == "") {
+            setPromptStatus(true)
+        }
+    }
+    return (
+        <div className=" w-[10%] h-[30px]  bg-zinc-200 rounded-full" onClick={hiddenPrompt} onBlur={showPrompt}>
+            <div className={`${promptStatus ? '' : 'hidden'}   pl-4 absolute bg-zinc-200 flex w-40 h-[30px] rounded-full items-center z-20   `}>
+                <SearchIcon className=" fill-zinc-400 w-[20px] h-[20px]" />
+                <p className=" text-zinc-400 text-sm">搜索音乐</p>
+            </div>
+            <div className=" absolute bg-zinc-200 rounded-full  w-40 flex z-10">
+                <input type="text" ref={inputRef} className="outline-none pl-3 h-[30px] rounded-full bg-zinc-200 w-[80%] block " />
+
+                <button className=" outline-none w-[20%] h-[30px]  flex items-center">
+                    <SearchIcon className="fill-zinc-400 w-[20px] h-[20px]" />
+                </button>
+            </div>
+        </div>
+    )
+}
